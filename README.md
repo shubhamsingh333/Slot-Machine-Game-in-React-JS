f3651112ad3c4ea73834b7519f84355fd4684611
shubhamsingh333 <shubhamsingh333333@gmail.com>
2021-04-24 00:48:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5zc39c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5zc39c)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="heading_style">
+        Welcome to <span style={{ fontWeight: "bold" }}>Slot Machine Game</span>
+      </h1>
+    </>
+  );
+};
+
+export default App;
