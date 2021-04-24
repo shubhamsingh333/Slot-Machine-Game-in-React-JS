c5dd289140acce29e8c6844aaed4dcf08790dd01
SHUS15EC <shubham.singh333333@gmail.com>
2021-04-25 03:09:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,8 @@
 import React from "react";
 import "./index.css";
+import SlotM from './SlotMachine';
 
-const SlotM = (props) => {
-  // let x = "😄";
-  // let y = "😄";
-  // let z = "😸";
 
-  let x= props.x;
-  let y= props.y;
-  let z= props.z;
-
-  if ((x === y) && (y === z)) {
-    return (
-      <>
-        <div className="slot_inner">
-          <h1>
-            {x} {y} {z}
-          </h1>
-          <h1> This is Matching</h1>
-          <hr/>
-        </div>
-      </>
-    );
-  }
-  else {
-    return (
-      <>
-        <div className="slot_inner">
-          <h1>
-            {x} {y} {z}
-          </h1>
-          <h1> This is Not Matching</h1>
-          <hr/>
-        </div>
-      </>
-    );
-
-  }
-};
 
 const App = () => {
   return (
